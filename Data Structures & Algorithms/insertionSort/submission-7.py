ff8cd5b908ec43ccb5266478ef451103332ebca3
AgentSamSA <samuelxlee@gmail.com>
2026-07-14 16:33:05 -0700

    Add: insertionSort - submission-7

---
+++ Data Structures & Algorithms/insertionSort/submission-7.py
@@ -0,0 +1,26 @@
+# Definition for a pair.
+# class Pair:
+#     def __init__(self, key: int, value: str):
+#         self.key = key
+#         self.value = value
+class Solution:
+    def insertionSort(self, pairs: List[Pair]) -> List[List[Pair]]:
+        states = []
+
+        temp = pairs.copy()
+
+        for i in range(len(temp)):
+            for j in range(i, 0, -1):
+                curr = temp[j]
+                prev = temp[j - 1]
+
+                if curr.key < prev.key:
+                    temp[j] = prev
+                    temp[j - 1] = curr
+                else:
+                    break
+
+            states.append(temp.copy())
+        
+        return states
+        
