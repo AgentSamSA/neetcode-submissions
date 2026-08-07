b1e36eeafd88ac29e5a19a5806d65923566553f0
AgentSamSA <samuelxlee@gmail.com>
2026-08-07 16:53:42 -0700

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        left = 0
+        max_profit = 0
+
+        for end in range(len(prices)):
+            profit = prices[end] - prices[left]
+            if profit > max_profit:
+                max_profit = profit
+            if prices[end] < prices[left]:
+                left = end
+        
+        return max_profit
