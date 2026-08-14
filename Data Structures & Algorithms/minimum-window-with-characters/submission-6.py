7e5a3f23cc64024c0ff70581596cfa4e0db8d599
AgentSamSA <samuelxlee@gmail.com>
2026-08-14 14:05:48 -0700

    Add: minimum-window-with-characters - submission-6

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-6.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(t) > len(s):
+            return ''
+
+        freqs_t = {}
+        freqs_window = {}
+        shortest = None
+        
+        for char in t:
+            freqs_t[char] = freqs_t.get(char, 0) + 1
+        
+        left = 0
+        satisfied = 0
+        for right in range(len(s)):
+            char = s[right]
+            if char in freqs_t:
+                freqs_window[char] = freqs_window.get(char, 0) + 1
+                if freqs_window[char] == freqs_t[char]:
+                    satisfied += 1
+
+            while satisfied == len(freqs_t):
+                if shortest is None or right - left + 1 <= len(shortest):
+                    shortest = s[left:right + 1]
+                if s[left] in freqs_t:
+                    freqs_window[s[left]] = freqs_window.get(s[left], 0) - 1
+                    if freqs_window[s[left]] < freqs_t[s[left]]:
+                        satisfied -= 1
+                left += 1
+        
+        return shortest or ''
