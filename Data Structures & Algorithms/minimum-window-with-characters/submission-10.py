e6561e9eff2d1e643ccd5d208aeade7334fd0578
AgentSamSA <samuelxlee@gmail.com>
2026-08-22 18:58:03 -0700

    Add: minimum-window-with-characters - submission-10

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-10.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(s) < len(t):
+            return ''
+        
+        freqs_t = {}
+        freqs_window = {}
+        substring = ''
+        substring_len = float('inf')
+
+        for char in t:
+            freqs_t[char] = freqs_t.get(char, 0) + 1
+
+        left = 0
+        satisfied = 0
+        for right in range(len(s)):
+            if s[right] in freqs_t:
+                freqs_window[s[right]] = freqs_window.get(s[right], 0) + 1
+                if freqs_window[s[right]] == freqs_t[s[right]]:
+                    satisfied += 1
+            
+            while satisfied == len(freqs_t):
+                if right - left + 1 < substring_len:
+                    substring = s[left:right + 1]
+                    substring_len = right - left + 1
+
+                if s[left] in freqs_t:
+                    freqs_window[s[left]] -= 1
+                    if freqs_window[s[left]] < freqs_t[s[left]]:
+                        satisfied -= 1
+                
+                left += 1
+        
+        return substring
